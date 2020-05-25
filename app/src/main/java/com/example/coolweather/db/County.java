300db3c291b969a143d0a72add4e8d21564834fe
guoxia <11117870@bbktel.com>
2020-05-25 14:38:39 +0800

    添加数据表映射的实体类以及litepal配置文件

---
+++ app/src/main/java/com/example/coolweather/db/County.java
@@ -0,0 +1,43 @@
+package com.example.coolweather.db;
+
+public class County {
+    private int id;
+    private String countyName;
+//    由于下面没有分级了，所以这里不需要代号了
+//    private int countyCode;
+//    该分级下面有天气情况
+    private int weatherId;
+    private int cityId;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getCountyName() {
+        return countyName;
+    }
+
+    public void setCountyName(String countyName) {
+        this.countyName = countyName;
+    }
+
+    public int getWeatherId() {
+        return weatherId;
+    }
+
+    public void setWeatherId(int weatherId) {
+        this.weatherId = weatherId;
+    }
+
+    public int getCityId() {
+        return cityId;
+    }
+
+    public void setCityId(int cityId) {
+        this.cityId = cityId;
+    }
+}
